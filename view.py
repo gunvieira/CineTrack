d36b5ac06a5d3b53a07eb8c3a34e9109fba7922a
gunvieira <gustavovieira132019@gmail.com>
2025-06-01 20:52:13 -0300

    adicionando tela atualizar

--- view.py
+++ view.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import tkinter as tk
-from controller import Controller
+from tkinter import messagebox
+
 
 class View:
 
@@ -15,14 +16,20 @@ class View:
         self.container = ctk.CTkFrame(self.root)
         self.container.pack(fill="both", expand=True)
 
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
         self.frameMenu = ctk.CTkFrame(self.container, fg_color="transparent")
         self.frameAdicionar = ctk.CTkFrame(self.container, fg_color="transparent")
+        self.frameAtualizar = ctk.CTkFrame(self.container, fg_color="transparent")
 
-        self.frameMenu.grid(row=0, column=0, sticky="nsew", padx=50, pady=30)
-        self.frameAdicionar.grid(row=0, column=0, sticky="nsew", padx=50, pady=30)
+        self.frameMenu.grid(row=0, column=0, sticky="nswe", padx=20, pady=20)
+        self.frameAdicionar.grid(row=0, column=0, sticky="nswe", padx=30, pady=10)
+        self.frameAtualizar.grid(row=0, column=0, sticky="nswe", padx=20, pady=10)
 
         self.telaMenu()
         self.telaAdicionar()
+        self.telaAtualizar()
 
         self.showTelamenu()
 
@@ -31,13 +38,20 @@ class View:
     def showTelamenu(self):
 
         self.frameMenu.tkraise()
+        self.root.geometry("450x500")
 
     def showTelaAdicionar(self):
 
         self.frameAdicionar.tkraise()
+        self.root.geometry("450x510")
+
+    def showTelaAtualizar(self):
+
+        self.frameAtualizar.tkraise()
+        self.root.geometry("400x415")
 
 
-    # ---------------MENU--------------------
+    # ---------------Menu--------------------
 
     def telaMenu(self):
         self.tituloMenu()
@@ -83,7 +97,8 @@ class View:
                                       text_color="black",
                                       fg_color="grey",
                                       hover_color="#9A9A9A",
-                                      cursor="hand2")
+                                      cursor="hand2",
+                                      command=self.showTelaAtualizar)
         btnAtualize.pack(pady=10)
 
         btnGeral = ctk.CTkButton(self.frameBotoesMenu,
@@ -134,7 +149,7 @@ class View:
         titulo = ctk.CTkLabel(self.frameTituloAdicionar,
                               text="Adicionar Novo Título",
                               font=ctk.CTkFont("Inter", 24, weight="bold"))
-        titulo.pack(anchor='e')
+        titulo.pack(anchor='w')
 
     def radioTipoAdicionar(self):
         frameRadioTipo = ctk.CTkFrame(self.frameAdicionar, fg_color="transparent")
@@ -200,7 +215,7 @@ class View:
         frameGenero.pack(anchor="w", side="right")
 
         nomeLabel = ctk.CTkLabel(frameNomeAdicionar,
-                                 text="Nome",
+                                 text="Nome:",
                                  font=ctk.CTkFont("Inter", 16))
         nomeLabel.pack(anchor="w")
 
@@ -211,7 +226,7 @@ class View:
         self.entryNome.pack()
 
         labelGenero = ctk.CTkLabel(frameGenero,
-                                   text="Gênero",
+                                   text="Gênero:",
                                    font=ctk.CTkFont("Inter", 16))
         labelGenero.pack(anchor="w")
 
@@ -252,7 +267,7 @@ class View:
         frameAno.pack(anchor="w", side="right")
 
         labelAno = ctk.CTkLabel(frameAno,
-                                text="Ano",
+                                text="Ano:",
                                 font=ctk.CTkFont("Inter", 16))
         labelAno.pack(anchor="w")
 
@@ -263,7 +278,7 @@ class View:
         self.entryAno.pack()
 
         labelStraming = ctk.CTkLabel(frameStreaming,
-                                       text="Streaming",
+                                       text="Streaming:",
                                        font=ctk.CTkFont("Inter", 16))
         labelStraming.pack(anchor="w")
 
@@ -291,7 +306,7 @@ class View:
         frameEpi.pack(side="right")
 
         labelTemp = ctk.CTkLabel(frameTemp,
-                                 text="Temporadas",
+                                 text="Temporadas:",
                                  font=ctk.CTkFont("Inter", 16))
         labelTemp.pack(side="left", anchor="w")
 
@@ -302,7 +317,7 @@ class View:
         self.entryTempAdicionar.pack(side="left", padx=(5, 0))
 
         labelEpi = ctk.CTkLabel(frameEpi,
-                                text="Ep./Temp.",
+                                text="Ep./Temp.:",
                                 font=ctk.CTkFont("Inter", 16))
         labelEpi.pack(side="left", anchor="w")
 
@@ -370,10 +385,10 @@ class View:
         frameNota.pack(anchor="w", fill="x", pady=(5, 10))
 
         labelAvalie = ctk.CTkLabel(frameNota,
-                                  text="Avalie esse título",
+                                  text="Avalie esse título:",
                                   font=ctk.CTkFont("Inter", 16),
                                   )
-        labelAvalie.pack(anchor="w")
+        labelAvalie.pack(anchor="w", side="left", padx=(0, 5))
 
         self.spinNotaAdicionarVar = tk.IntVar(value=10)
 
@@ -384,15 +399,16 @@ class View:
                         textvariable=self.spinNotaAdicionarVar,
                         width=2,
                         fg="black",
-                        bg="grey",
+                        bg="#AFB4BC",
                         font=("Inter", 12),
                         justify="center",
+                        text="Sua nota",
                         relief="groove")
         spinNota.pack(anchor="w")
 
     def botoesAdicionar(self):
         frameBotoesAdicionar = ctk.CTkFrame(self.frameAdicionar, fg_color="transparent")
-        frameBotoesAdicionar.pack(anchor='center', fill="x", pady=(10, 10))
+        frameBotoesAdicionar.pack(anchor="center", pady=(10, 10))
 
         #trocar hover
 
@@ -423,6 +439,240 @@ class View:
                                   command=lambda: print("Salvar"))
         btnSalvar.pack(side="left")
 
+# ---------------Atualizar Título--------------------
+
+    def telaAtualizar(self):
+        self.tituloAtualizar()
+        self.tipoAtualizar()
+        self.selecaoSerieAtualizar()
+        self.selecaoTempEpisAtualizar()
+        self.statusAtualizar()
+        self.notaAtualizar()
+
+        self.botoesAtualizar()
+
+    def tituloAtualizar(self):
+        frameTitulo = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameTitulo.pack(pady=(5, 10), anchor="w")
+
+        label = ctk.CTkLabel(frameTitulo,
+                             text="Atualize seu Progresso",
+                             font=ctk.CTkFont("Inter", 24, weight="bold"))
+        label.pack(anchor="w")
+
+    def tipoAtualizar(self):
+        frame_tipo = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frame_tipo.pack(anchor="w", pady=(5, 10))
+
+        label = ctk.CTkLabel(frame_tipo,
+                             text="O que deseja atualizar:",
+                             font=ctk.CTkFont("Inter", 16))
+        label.pack(anchor="w")
+
+        self.tipoAtualizarVar = ctk.StringVar(value="Filme")
+
+        radio_filme = ctk.CTkRadioButton(frame_tipo, text="Filme",
+                                         variable=self.tipoAtualizarVar,
+                                         value="Filme",
+                                         font=ctk.CTkFont("Inter", 15),
+                                         radiobutton_width=20,
+                                         radiobutton_height=20,
+                                         border_width_unchecked=3,
+                                         border_width_checked=7,
+                                         fg_color="#414141",
+                                         hover_color="#6F6F83",
+                                         border_color="grey")
+        radio_filme.pack(side="left", padx=6)
+
+        radio_serie = ctk.CTkRadioButton(frame_tipo, text="Série",
+                                         variable=self.tipoAtualizarVar,
+                                         value="Série",
+                                         font=ctk.CTkFont("Inter", 15),
+                                         radiobutton_width=20,
+                                         radiobutton_height=20,
+                                         border_width_unchecked=3,
+                                         border_width_checked=7,
+                                         fg_color="#414141",
+                                         hover_color="#6F6F83",
+                                         border_color="grey")
+        radio_serie.pack(side="left")
+
+    def selecaoSerieAtualizar(self):
+        frameSerie = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameSerie.pack(anchor="w", pady=(5, 10))
+
+        label = ctk.CTkLabel(frameSerie,
+                             text="Selecione a série que deseja atualizar:",
+                             font=ctk.CTkFont("Inter", 16))
+        label.pack(anchor="w")
+
+        self.seriesDisponiveis = ["Dark", "Wandinha", "Black Mirror"]
+
+        self.comboSerie = ctk.CTkOptionMenu(frameSerie,
+                                                values=self.seriesDisponiveis,
+                                                dropdown_font=ctk.CTkFont("Inter", 12),
+                                                font=ctk.CTkFont("Inter", 12, weight="bold"),
+                                                fg_color="grey",
+                                                button_color="#656565",
+                                                button_hover_color="#414141",
+                                                dropdown_fg_color="#AFB4BC",
+                                                dropdown_hover_color="grey",
+                                                dropdown_text_color="black")
+        self.comboSerie.pack(side="left")
+
+    def selecaoTempEpisAtualizar(self):
+        frameTempEpi = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameTempEpi.pack(anchor="w", pady=(10, 0))
+
+        labelTemp = ctk.CTkLabel(frameTempEpi,
+                                 text="Temporada:",
+                                 font=ctk.CTkFont("Inter", 16))
+        labelTemp.pack(side="left", padx=(0, 2))
+
+        self.tempAtualizarVar = tk.IntVar(value=1)
+        self.qtdTempAtualizar = 3
+
+        spinTemp = tk.Spinbox(frameTempEpi,
+                              from_=1,
+                              to=self.qtdTempAtualizar,
+                              textvariable=self.tempAtualizarVar,
+                              width=2,
+                              fg="black",
+                              bg="#AFB4BC",
+                              font=("Inter", 12),
+                              justify="center",
+                              relief="groove")
+        spinTemp.pack(side="left")
+
+        labelEpi = ctk.CTkLabel(frameTempEpi,
+                                text="Episódio:",
+                                font=ctk.CTkFont("Inter", 16))
+        labelEpi.pack(side="left", padx=(10, 2))
+
+        self.epiAtualizarVar = tk.IntVar(value=1)
+        self.qtdEpiAtualizar = 12
+
+        spin_epi = tk.Spinbox(frameTempEpi,
+                              from_=1,
+                              to=self.qtdEpiAtualizar,
+                              textvariable=self.epiAtualizarVar,
+                              width=2,
+                              fg="black",
+                              bg="#AFB4BC",
+                              font=("Inter", 12),
+                              justify="center",
+                              relief="groove")
+        spin_epi.pack(side="left")
+
+    def notaAtualizar(self):
+        frameNota = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameNota.pack(anchor="w", fill="x", pady=(10, 10))
+
+        labelAvalie = ctk.CTkLabel(frameNota,
+                                   text="Avalie esse título:",
+                                   font=ctk.CTkFont("Inter", 16),
+                                   )
+        labelAvalie.pack(anchor="w", side="left", padx=(0, 5))
+
+        self.spinNotaAtulizarVar = tk.IntVar(value=10)
+
+        spinNota = tk.Spinbox(
+            frameNota,
+            from_=1,
+            to=10,
+            textvariable=self.spinNotaAtulizarVar,
+            width=2,
+            fg="black",
+            bg="#AFB4BC",
+            font=("Inter", 12),
+            justify="center",
+            relief="groove")
+        spinNota.pack(anchor="w")
+
+    def statusAtualizar(self):
+        frameStatus = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameStatus.pack(anchor="w", pady=(10, 10))
+
+        labelStatus = ctk.CTkLabel(frameStatus, text="Status:", font=ctk.CTkFont("Inter", 16))
+        labelStatus.pack(anchor="w")
+
+        frameRadioStatus = ctk.CTkFrame(frameStatus, fg_color="transparent")
+        frameRadioStatus.pack(anchor="w")
+
+        self.statusAtualizarVar = ctk.StringVar(value="Assistindo")
+
+        radioAssistindo = ctk.CTkRadioButton(frameRadioStatus,
+                                              text="Assistindo",
+                                              variable=self.statusAtualizarVar,
+                                              value="Assistindo",
+                                              font=ctk.CTkFont("Inter", 14),
+                                              radiobutton_width=20,
+                                              radiobutton_height=20,
+                                              border_width_unchecked=3,
+                                              border_width_checked=7,
+                                              fg_color="#414141",
+                                              hover_color="#6F6F83",
+                                              border_color="grey")
+        radioAssistindo.pack(side="left", padx=10)
+
+        radioConcluido = ctk.CTkRadioButton(frameRadioStatus,
+                                             text="Concluído",
+                                             variable=self.statusAtualizarVar,
+                                             value="Concluído",
+                                             font=ctk.CTkFont("Inter", 14),
+                                             radiobutton_width=20,
+                                             radiobutton_height=20,
+                                             border_width_unchecked=3,
+                                             border_width_checked=7,
+                                             fg_color="#414141",
+                                             hover_color="#6F6F83",
+                                             border_color="grey")
+        radioConcluido.pack(side="left")
+
+    def botoesAtualizar(self):
+        frameBotoes = ctk.CTkFrame(self.frameAtualizar, fg_color="transparent")
+        frameBotoes.pack(pady=10)
+
+        btnVoltar = ctk.CTkButton(frameBotoes,
+                                  text="Voltar",
+                                  width=80,
+                                  fg_color="#414141",
+                                  hover_color="#9A9A9A",
+                                  font=ctk.CTkFont("Inter", 16),
+                                  command=self.showTelamenu)
+        btnVoltar.pack(side="left", padx=5)
+
+        btnLimpar = ctk.CTkButton(frameBotoes,
+                                  text="Limpar",
+                                  width=80,
+                                  fg_color="#414141",
+                                  hover_color="#9A9A9A",
+                                  font=ctk.CTkFont("Inter", 16),
+                                  command=lambda: print("Limpar"))
+        btnLimpar.pack(side="left", padx=5)
+
+        btnDeletar = ctk.CTkButton(frameBotoes,
+                                   text="Deletar",
+                                   width=80,
+                                   fg_color="#414141",
+                                   hover_color="#9A9A9A",
+                                   font=ctk.CTkFont("Inter", 16),
+                                   command=lambda: print("Deletar"))
+        btnDeletar.pack(side="left", padx=5)
+
+        btnAtualizar = ctk.CTkButton(frameBotoes,
+                                     text="Atualizar",
+                                     width=80,
+                                     fg_color="#414141",
+                                     hover_color="#9A9A9A",
+                                     font=ctk.CTkFont("Inter", 16),
+                                     command=lambda: messagebox.showinfo("CineTrack", "O título foi Atualizado."))
+        btnAtualizar.pack(side="left", padx=5)
+
+
 if __name__ == "__main__":
     View()
+
+
+
 
